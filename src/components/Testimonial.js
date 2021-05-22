d135bba7adac55c0e8a4e95014a6324099b22bc5
Hyunji Kim <69276681+creativehkim@users.noreply.github.com>
2021-05-22 19:35:12 -0400

    testminonial carousel added using SwiperJs

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -5,13 +5,16 @@ const Testimonial = ({ src, name, text}) => {
   return (
     <>
       <div className="testimonial-card-wrapper">
-        <img src={src} alt="avatar" />
+        <img className="image" src={src} alt="avatar" />
         <h3 className='name'>{name}</h3>
         <p className='text'>{text}</p>
       </div>
 
     </>
   )
+  console.log(src)
 }
+
+
 
 export default Testimonial
--- src/components/TestimonialSection.js
+++ src/components/TestimonialSection.js
@@ -1,50 +1,48 @@
-import React, { useState, useEffect} from 'react';
-import { Button } from './Button';
+import React from 'react'
+import { Button } from './Button'
 import { TestimonialData } from './TestimonialData'
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper.scss'
+import SwiperCore, { Autoplay } from 'swiper/core';
 import Testimonial from './Testimonial'
 import './TestimonialSection.css'
 
 
-function TestimonialSlider({ testimonials }) {
-  const [x, setX] = useState(0)
-  const length = testimonials.length
+SwiperCore.use(Autoplay)
 
-  let interval;
-
-  useEffect(() => {
-    setInterval(() => {setX(x => x + 100)}, 3000)
-  }, [])
-
-  const goLeft = () => {
-    clearInterval(interval)
-    x === 0 ? setX(-100 * (length -1)) : setX(x + 100)
-  }
-
-  console.log(goLeft)
-
-  if (!Array.isArray(testimonials) || length <=0) {
-    return null
-  }
+function TestimonialSlider() {
 
   return (
     <>
       <div className='testimonial-section'>
         <div className='testimonial-container'>
           <h1 className='heading'>What they’ve said</h1>
-          <div className='slider'>
-            {TestimonialData.map((testimonial, index) => {
-              return (
-                <Testimonial 
-                  className="testimonial-card"
-                  text={testimonial.text}
-                  name={testimonial.name}
-                  src={testimonial.image} 
-                />
-              )
-            })}
-
-          </div>
+          <Swiper
+            centeredSlides={true}
+            loop={true}
+            autoplay={{"delay": 2500, "disableOnInteraction": false}}
+            slidesPerView={3}
+            spaceBetween={30}
+            onSlideChange={()=> console.log('slide change')}
+            onSwiper={swiper => console.log(swiper)}
+            tag='div'
+            wrapperTag='ul'
+            >
+            
+              {TestimonialData.map((testimonial) => {
+                return (
+                  <SwiperSlide key={testimonial.id} tag='li'><Testimonial 
+                    className="testimonial-card"
+                    text={testimonial.text}
+                    name={testimonial.name}
+                    src={testimonial.image} 
+                  /></SwiperSlide>
+                  
+                  )
+                  
+              })}
+              
+          </Swiper>
           <Button className='testimonial-btn' buttonStyle='btn-primary'>Get Started</Button>         
         </div>
       </div>
