b78ab719ace4457d9024cd6fae769e46d35ba04b
Hyunji Kim <creative.hkim@gmail.com>
2021-05-23 11:43:41 -0400

    fixed the image problem

--- src/components/TestimonialData.js
+++ src/components/TestimonialData.js
@@ -1,25 +1,30 @@
+import Img1 from '../images/avatar-ali.png'
+import Img2 from '../images/avatar-anisha.png'
+import Img3 from '../images/avatar-richard.png'
+import Img4 from '../images/avatar-shanai.png'
+
 export const TestimonialData = [
   {
     name: 'Ali Bravo',
-    image: require('../images/avatar-ali.png'),
+    image: Img1,
     text: 'We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.',
     id: 1
   },
   {
     name: 'Anisha Li',
-    image: '../images/avatar-anisha.png',
+    image: Img2,
     text: 'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.',
     id: 2
   },
   {
     name: 'Richard Watts',
-    image: '../images/avatar-richard.png',
+    image: Img3,
     text: 'Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!',
     id: 3
   },
   {
     name: 'Shanai Gough',
-    image: '../images/avatar-shanai.png',
+    image: Img4,
     text: 'Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.',
     id: 4
   }
